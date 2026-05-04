d4a48cbd5a7a6d86ca6f5f4052ef724b6e847a0a
Carlos <carlos@MacBook-Pro-de-Carlos.local>
2026-05-04 17:42:53 +0200

    preuba

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 export default function Index() {
     return (
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Text>🔥 ESTO TIENE QUE CAMBIAR 🔥</Text>
+            <Text>🔥 ESTO TIENE QUE CAMBIAR 12 🔥</Text>
         </View>
     );
 }
